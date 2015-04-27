e5800c9451b9848ebd0abb27f8438798288ac27c
Ramon Schauer <ramon25@gmx.de>
2015-04-27 11:51:20 +0200

    practical 2

---
+++ Assets/Scripts/slingshot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slingshot : MonoBehaviour {
+
+	private GameObject launchpoint;
+
+	void Awake(){
+		Transform launchpointTrans = transform.Find("LaunchPoint");
+		launchpoint = launchpointTrans.gameObject;
+		launchpoint.SetActive (false);
+	}
+
+	void OnMouseEnter(){
+	print ("Slingshotonmouseenterstuff");
+		launchpoint.SetActive (true);
+		     }
+
+	void OnMouseExit(){
+		print ("exitstuff");
+		launchpoint.SetActive (false);
+	}
+}
